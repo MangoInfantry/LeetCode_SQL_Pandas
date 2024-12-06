f306f37e298d3938b9da6325f719350a675486c5
Soomin Choi <133320806+MangoInfantry@users.noreply.github.com>
2024-12-06 13:35:11 +0900

    Time: 416 ms (94.31%), Space: 69.8 MB (91.19%) - LeetHub

---
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.py
@@ -0,0 +1,26 @@
+# 월별, 국가별 거래 건수 및 총액, 승인된 거래 건수 및 총액을 찾는 솔루션을 작성
+# 결과 테이블을 임의의 순서로 반환
+
+import pandas as pd
+import numpy as np
+
+def monthly_transactions(transactions):
+    transactions['month'] = transactions['trans_date'].dt.strftime('%Y-%m')
+    transactions['country'].fillna('null',inplace=True)
+    transactions['approved'] = np.where(transactions['state'] == 'approved',transactions['amount'],nan)
+    
+    trans_count = transactions.groupby(['month', 'country'])['state'].count()
+    approved_count = transactions[transactions['state'] == 'approved'].groupby(['month', 'country'])['state'].count()
+    trans_total_amount = transactions.groupby(['month', 'country'])['amount'].sum()
+    approved_total_amount = transactions[transactions['state'] == 'approved'].groupby(['month', 'country'])['amount'].sum()
+    
+    table = pd.DataFrame({
+        'trans_count': trans_count,
+        'approved_count': approved_count,
+        'trans_total_amount': trans_total_amount,
+        'approved_total_amount': approved_total_amount
+    }).reset_index().fillna(0)
+    table['country'].replace('null',nan,inplace=True)
+    return table
+
+    
