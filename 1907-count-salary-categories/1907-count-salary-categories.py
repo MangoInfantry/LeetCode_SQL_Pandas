ff8cc4d10243cfc0e107715df2c337afd8c4731b
Soomin Choi <133320806+MangoInfantry@users.noreply.github.com>
2024-12-08 15:30:05 +0900

    Time: 330 ms (98.23%), Space: 90 MB (92.2%) - LeetHub

---
+++ 1907-count-salary-categories/1907-count-salary-categories.py
@@ -0,0 +1,19 @@
+# 각 급여 범주에 대한 은행 계좌 수를 계산하는 솔루션을 작성하세요. 급여 카테고리는 다음과 같습니다:
+# “낮은 급여": 모든 급여가 $20000 미만입니다.
+# “평균 급여": 포함 범위[$20000, $50000]의 모든 급여입니다.
+# “높은 연봉": $50000보다 엄격하게 큰 모든 급여.
+# 결과 테이블에는 세 가지 카테고리가 모두 포함되어야 합니다. 한 카테고리에 계정이 없는 경우 0을 반환합니다.
+# 결과 테이블은 어떤 순서로든 반환합니다.
+# 결과 형식은 다음 예시와 같습니다.
+
+import pandas as pd
+
+def count_salary_categories(accounts):
+    low_salary =  len(accounts[accounts['income']<20000])
+    average_salary = len(accounts[(accounts['income']>=20000) & (accounts['income']<=50000)])
+    high_salary = len(accounts[accounts['income']>50000])
+    data = pd.DataFrame({'category':['High Salary', 'Average Salary', 'Low Salary'],
+                         'accounts_count': [high_salary, average_salary, low_salary]})
+    return data
+
+    
