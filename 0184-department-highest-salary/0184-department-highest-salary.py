80819026ada53c979c6b9950e399c839fcde6812
Soomin Choi <133320806+MangoInfantry@users.noreply.github.com>
2025-01-01 18:34:32 +0900

    Time: 461 ms (43.86%), Space: 68.6 MB (67.58%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.py
+++ 0184-department-highest-salary/0184-department-highest-salary.py
@@ -1,11 +1,14 @@
+# 각 부서에서 가장 높은 연봉을 받는 직원을 찾는 솔루션을 작성합니다.
+# 결과 테이블을 임의의 순서로 반환합니다.
+# 결과 형식은 다음 예제와 같습니다.
+
 import pandas as pd
 
 def department_highest_salary(employee, department):
-    employee = employee.rename(columns={'name':'Employee'})
-    department = department.rename(columns={'name':'Department'})
-    table = pd.merge(employee, department, left_on='departmentId',right_on = 'id', how='left')
-    table = table.rename(columns={'id_x':'id'}).drop(columns={'id_y'})
-    temp = table.groupby(['Department'])['salary'].max().reset_index()
-    table = table.merge(temp, how='inner', on = ['salary','Department'])
-    table = table.rename(columns={'salary':'Salary'})
-    return table[['Department','Employee','Salary']]
+    department.rename(columns={'name':'Department'}, inplace=True)
+    table = pd.merge(employee, department, left_on='departmentId', right_on='id')
+    table['rank'] = table.groupby(['Department'])['salary'].rank(method='dense', ascending=False)
+    result = table[table['rank']==1]
+    result.rename(columns={'salary':'Salary','name':'Employee'}, inplace=True)
+    return result[['Department','Employee','Salary']]
+    
