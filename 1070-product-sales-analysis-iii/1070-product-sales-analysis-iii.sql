fe0ead3d0c40240f65dc7787b03477f5a7d3bda0
Soomin Choi <133320806+MangoInfantry@users.noreply.github.com>
2025-01-08 17:53:01 +0900

    Time: 1057 ms (75.33%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,17 +1,21 @@
-# Write your MySQL query statement below
-# 판매된 모든 제품의 첫 해의 제품 id, 연도, 수량, 가격을 출력
-
+# 판매된 모든 제품의 첫해에 대한 제품 ID, 연도, 수량 및 가격을 선택하는 솔루션을 작성합니다.
+# 결과 테이블을 임의의 순서로 반환합니다.
+# 결과 형식은 다음 예제와 같습니다.
+# sales 테이블만 이용하면 됩니다.
+# window function의 dense_rank를 이용하여 제품 별 연도를 정렬합니다.
+# rnk가 1인 것만 출력하도록 합니다.  
 with cte as (
-    select s.product_id,
-       year,
-       quantity,
-       price,
-       rank () over (partition by s.product_id order by year asc) as rnk
+    select product_id,
+           dense_rank() over (partition by product_id order by year) as rnk,
+           year,
+           quantity,
+           price
     from sales as s
-    join product as p
-    on s.product_id = p.product_id 
 )
 
-select product_id, year as first_year, quantity, price 
+select product_id,
+       year as first_year,
+       quantity,
+       price
 from cte
-where rnk = 1
+where rnk=1
