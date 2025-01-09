178a85d2d679a26201c5d93f3d8b67f5b4e884b1
Soomin Choi <133320806+MangoInfantry@users.noreply.github.com>
2025-01-09 23:35:31 +0900

    Time: 2496 ms (14.31%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,30 +1,25 @@
 # Write your MySQL query statement below
-# 가장 많은 수의 영화에 별점을 준 사용자의 이름을 찾기 동점인 경우 사전적으로 더 순서가 낮은 이름을 반환
-# 2020년 2월에 평균 평점이 가장 높은 영화 이름, 동점일 경우 사전적으로 더 순서가 낮은 이름을 반환
-# cte1에서 users와 movierating 테이블을 조인한다 -> 그다음 group by를 써서 name과 name의 카운트를 출력한다. 
-# cte2에서 movies와 movierating 테이블을 조인한다 -> 그다음 group by를 써서 평균 평점을 구한다.
-# 두 테이블을 union all한다.
+# 가장 많은 수의 영화에 별점을 준 사용자의 이름을 찾습니다. 동점인 경우 사전적으로 더 작은 사용자 이름을 반환합니다.
+# 2020년 2월에 평균 평점이 가장 높은 영화 이름을 찾습니다. 동점일 경우 사전적으로 더 작은 영화 이름을 반환합니다.
+# 결과 형식은 다음 예시와 같습니다.
 
+# 이름과 window function을 이용하여 이름이 몇번 세어졌는지를 첫번째 cte문으로 한다.  
 with cte as (
-    select name, count(name) as cnt 
+    select distinct name, count(name) over (partition by name) as cnt
     from users as u
-    join movierating as m 
+    right join movierating as m
     on u.user_id = m.user_id
-    group by name
-    order by cnt desc, name asc
+    order by cnt desc, name
     limit 1
-), 
-cte2 as (
-    select title, avg(rating) as avg_rate
-    from movies as m
-    join movierating as r
-    on m.movie_id = r.movie_id
-    where left(created_at,7) = '2020-02'
-    group by title
-    order by avg_rate desc, title asc 
+), cte2 as (
+    select distinct title, avg(rating) over (partition by title) as rate
+    from movierating as r
+    left join movies as m
+    on r.movie_id = m.movie_id
+    where left(created_at, 7) = '2020-02'
+    order by rate desc, title 
     limit 1
 )
-
 
 select name as results 
 from cte
