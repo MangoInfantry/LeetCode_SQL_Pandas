ec45d155c91b24b740aea33512763fbcefdbf271
Soomin Choi <133320806+MangoInfantry@users.noreply.github.com>
2025-02-25 11:16:54 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,16 +1,17 @@
 # Write your MySQL query statement below
-# 어떤 주문이 모든 첫 주문들 중에서 immediate 판정을 받는 주문일 확률을 구하라 
-# 먼저 첫 주문을 구한다. (dense_rank를 써서 1인 것들만 출력)
-# 첫 주문들 중에 case when을 써서 order_date와 customer_pref가 같은 것들을 고르고 전체 주문 비율로 나눈다.
+-- 고객이 선호하는 배송 날짜가 주문 날짜와 동일한 경우 즉시 주문이라고 하며, 그렇지 않은 경우 예약 주문이라고 합니다.
+-- 고객의 첫 주문은 고객이 주문한 주문 중 주문 날짜가 가장 빠른 주문입니다. 고객에게 정확히 하나의 첫 주문이 있다는 것은 보장됩니다.
+-- 모든 고객의 첫 주문에서 즉시 주문의 비율을 소수점 둘째 자리에서 반올림하여 구하는 솔루션을 작성합니다.
+-- 결과 형식은 다음 예제와 같습니다.
+
+-- first order이 즉시 배송인지, 아니면 예약 배송인지 확인해야하기 때문에, dense_rank() 윈도우 함수를 매겨서 1순위인 것만 뽑아냄
+-- first order의 sum(case when ~ ) 을 이용해서 order_date가 customer_pref_delivery_date와 같은 지 조건걸어서 한 다음, 총 주문 개수로 나눠서 퍼센트 출력
 
 with cte as (
-    select delivery_id,
-        customer_id,
-        order_date,
-        customer_pref_delivery_date,
-        dense_rank() over (partition by customer_id order by order_date) as rnk
+    select order_date, customer_pref_delivery_date, dense_rank() over (partition by customer_id order by order_date) as rnk  
     from delivery
 )
-select 100 * round(count(case when order_date = customer_pref_delivery_date then 1 end)/count(delivery_id),4) as immediate_percentage
-from cte 
-where rnk = 1
+
+select 100.0 * round(sum(case when order_date = customer_pref_delivery_date then 1 else 0 end)/count(*),2) as immediate_percentage
+from cte
+where rnk = 1 
