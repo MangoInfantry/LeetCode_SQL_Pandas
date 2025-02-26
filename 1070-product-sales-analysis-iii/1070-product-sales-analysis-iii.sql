bb7fcb3e25929ddce2d612b32319e3ccc9d1be48
Soomin Choi <133320806+MangoInfantry@users.noreply.github.com>
2025-02-26 10:22:07 +0900

    Time: 1708 ms (60.24%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,21 +1,14 @@
-# 판매된 모든 제품의 첫해에 대한 제품 ID, 연도, 수량 및 가격을 선택하는 솔루션을 작성합니다.
-# 결과 테이블을 임의의 순서로 반환합니다.
-# 결과 형식은 다음 예제와 같습니다.
-# sales 테이블만 이용하면 됩니다.
-# window function의 dense_rank를 이용하여 제품 별 연도를 정렬합니다.
-# rnk가 1인 것만 출력하도록 합니다.  
+-- 판매된 모든 제품의 첫해에 대한 제품 ID, 연도, 수량 및 가격을 선택하는 솔루션을 작성합니다.
+-- 결과 테이블을 임의의 순서로 반환합니다.
+-- 결과 형식은 다음 예제와 같습니다.
+
+-- cte문 안에 product_id에 따른 year의 dense_rank()를 적용시킨 후, product_id, year, quantity, price를 출력
+
 with cte as (
-    select product_id,
-           dense_rank() over (partition by product_id order by year) as rnk,
-           year,
-           quantity,
-           price
-    from sales as s
+    select product_id, dense_rank() over (partition by product_id order by year asc) as rnk, year, quantity, price
+    from sales
 )
 
-select product_id,
-       year as first_year,
-       quantity,
-       price
-from cte
-where rnk=1
+select product_id, year as first_year, quantity, price
+from cte 
+where rnk = 1
